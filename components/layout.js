a7ddcf215c71494dacc108b6fa375da8aa66f5cd
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-14 09:01:55 +0700

    add referensi

--- components/layout.js
+++ components/layout.js
@@ -23,6 +23,7 @@ export default function Layout({ children, home }) {
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
         <meta name="og:title" content={siteTitle} />
+        <title>title ini akan di overwrite ketika di page /</title>
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
       <header className={styles.header}>
