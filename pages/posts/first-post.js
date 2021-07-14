fa2f73f1c0e66a89e7a52ad10d32a689608ace04
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-14 07:59:50 +0700

    add example using head tag

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,13 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First post</title>
+      </Head>
       <h1>First Post</h1>
       <Image src="/profile.jpg" alt="profile" width={200} height={200} />
       <h2>
