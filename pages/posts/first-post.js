24fb690d4239562cc87e8f414229cbd1982c9467
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-14 07:51:59 +0700

    add example using image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
+      <Image src="/profile.jpg" alt="profile" width={200} height={200} />
       <h2>
         <Link href="/">
           <a>Back to home</a>
