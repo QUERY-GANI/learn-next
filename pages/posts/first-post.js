847cc22e18850021d89189446b60d24a117ff1e5
QUERY-GANI <ahmadxgani01@gmail.com>
2021-07-14 08:57:30 +0700

    refactoring | polish page

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,22 +1,15 @@
-import Link from 'next/link'
 import Image from 'next/image'
-import Head from 'next/head'
 import Layout from "../../components/layout";
+
+export const siteTitle = 'Next.js Sample Website'
 
 export default function FirstPost() {
   return (
     <Layout>
-      <Head>
-        <title>First post</title>
-      </Head>
       <h1 className="test">First Post</h1>
       <Image src="/profile.jpg" alt="profile" width={200} height={200} layout="responsive"/>
       <h2>
-        <Link href="/">
-          <a>Back to home</a>
-        </Link>
-          <br/>
-          <a href="https://github.com/query-gani">Visit my profile</a>
+        <a href="https://github.com/query-gani">Visit my profile</a>
       </h2>
     </Layout>
   )
